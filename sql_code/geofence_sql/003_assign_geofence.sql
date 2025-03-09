ab750975b7146de2166b8f2218d24d968efbbd8e
Soei Kanemura <soei.kanemura@schemeverge.com>
2025-03-09 20:58:51 +0900

    Auto-commit: 2025-03-09 20:58:51

--- sql_code/geofence_sql/003_assign_geofence.sql
+++ sql_code/geofence_sql/003_assign_geofence.sql
@@ -7,6 +7,6 @@ SELECT
     TIMESTAMP(
         CONCAT(d.year, '-', d.month, '-', d.day, ' ', d.hour, ':', d.minute, ':00')
     ) AS timestamp
-FROM `your_project.dataset.raw_location_data` AS d
-JOIN `your_project.dataset.geofences` AS g
+FROM `rd-dapj-dev.raw_daimaruyu_data.{TABLE_NAME}` AS d
+JOIN `rd-dapj-dev.raw_daimaruyu_data.dmy_buil_geojson_csv` AS g
 ON ST_CONTAINS(g.region, ST_GEOGPOINT(d.longitude, d.latitude));
