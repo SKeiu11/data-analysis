239891926da22f915a702805b652b4f18ed70856
Soei Kanemura <soei.kanemura@schemeverge.com>
2025-03-10 22:15:47 +0900

    Auto-commit: 2025-03-10 22:15:47

---
+++ temp.sql
@@ -0,0 +1,13 @@
+CREATE OR REPLACE TABLE `rd-dapj-dev.raw_daimaruyu_data.PDP_20231016_distance` AS
+WITH home_locations AS (
+    SELECT 
+        home_prefcode,
+        home_citycode,
+        ST_GEOGPOINT(longitude, latitude) AS home_location
+    FROM `rd-dapj-dev.raw_daimaruyu_data.home_location_mapping`
+)
+SELECT
+    d.*,
+    ST_DISTANCE(h.home_location, ST_GEOGPOINT(139.767125, 35.681236)) / 1000 AS distance_from_home  -- km単位
+FROM `rd-dapj-dev.raw_daimaruyu_data.PDP_20231016_transportation` AS d
+LEFT JOIN home_locations AS h ON d.home_prefcode = h.home_prefcode AND d.home_citycode = h.home_citycode;
